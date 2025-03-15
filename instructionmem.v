5c5c5bb4aa26bd6edaf3747986d1547ddde626e2
Umang Khandelwal <umang.2023ug2004@iiitranchi.ac.in>
2025-03-15 16:07:26 +0530

    Update instructionmem.v

--- instructionmem.v
+++ instructionmem.v
@@ -1,12 +1,1 @@
-module instruction_memory (
-    input [31:0] address,  
-    output reg [31:0] instruction 
-);
-    reg [7:0] memory [0:1023]; 
-    initial begin
-        $readmemh("instructions.hex", memory); 
-    end
-    always@(*) begin
-        instruction = {memory[address], memory[address + 1], memory[address + 2], memory[address + 3]};
-    end
-endmodule
+

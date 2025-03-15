f2c58d93166e69b5402d1a38269bec9f5421d6ed
Umang Khandelwal <umang.2023ug2004@iiitranchi.ac.in>
2025-03-15 15:27:09 +0530

    Update instructionmem.v

--- instructionmem.v
+++ instructionmem.v
@@ -9,14 +9,4 @@ module instruction_memory (
     always@(*) begin
         instruction = {memory[address], memory[address + 1], memory[address + 2], memory[address + 3]};
     end
-    integer i=0;
-    initial begin
-        $display("Instruction at 0x00: %h", {memory[i], memory[i+1], memory[i+2], memory[i+3]});
-        i=i+4;
-        $display("Instruction at 0x04: %h", {memory[i], memory[i+1], memory[i+2], memory[i+3]});
-        i=i+4;
-        $display("Instruction at 0x08: %h", {memory[i], memory[i+1], memory[i+2], memory[i+3]});
-        i=i+4;
-        $display("Instruction at 0x08: %h", {memory[i], memory[i+1], memory[i+2], memory[i+3]});
-    end
 endmodule
